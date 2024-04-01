d433319d260ea38d618545d5592848b00441a366
Dimitreee <danewdima@gmail.com>
2024-04-01 13:15:04 +0300

    cleanup

--- src/components/Navigation/index.tsx
+++
@@ -1,6 +0,0 @@
-export type { BreadcrumbsProps } from './Breadcrumbs/Breadcrumbs';
-export { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
-export type { LinkProps } from './Link/Link';
-export { Link } from './Link/Link';
-export type { SegmentedControlProps } from './SegmentedControl/SegmentedControl';
-export { SegmentedControl } from './SegmentedControl/SegmentedControl';
--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Blocks';
-export * from './Feedback';
-export * from './Form';
-export * from './Misc';
-export * from './Navigation';
-export * from './Service';
-export * from './Typography';
